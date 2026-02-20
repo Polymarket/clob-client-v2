abe51869a8120f7a1dd7abd8d21d78a9fa3cd414
pradeep-selva <pradeepselvaraju@gmail.com>
2026-02-20 12:52:19 +0530

    rf: minor typing fix in orders

--- src/order-builder/helpers/createMarketOrder.ts
+++ src/order-builder/helpers/createMarketOrder.ts
@@ -2,7 +2,7 @@ import type { JsonRpcSigner } from "@ethersproject/providers";
 import type { Wallet } from "@ethersproject/wallet";
 import { zeroAddress } from "viem";
 import { getContractConfig } from "../../config";
-import type { SignatureTypeV2, SignedOrderV1, SignedOrderV2 } from "../../order-utils";
+import type { OrderDataV1, SignatureTypeV2, SignedOrderV1, SignedOrderV2 } from "../../order-utils";
 import type { Chain, CreateOrderOptions, UserMarketOrderV2 } from "../../types";
 import { buildMarketOrderCreationArgs } from "./buildMarketOrderCreationArgs";
 import { buildOrder } from "./buildOrder";
@@ -38,7 +38,7 @@ export const createMarketOrder = async (
 				? contractConfig.negRiskExchange
 				: contractConfig.exchange;
 			// Add taker field for V1 orders (V1 requires it, V2 does not)
-			(orderData as any).taker = zeroAddress;
+			(orderData as OrderDataV1).taker = zeroAddress;
 			break;
 		case 2:
 			exchangeContract = options.negRisk
--- src/order-builder/helpers/createOrder.ts
+++ src/order-builder/helpers/createOrder.ts
@@ -2,7 +2,7 @@ import type { JsonRpcSigner } from "@ethersproject/providers";
 import type { Wallet } from "@ethersproject/wallet";
 import { zeroAddress } from "viem";
 import { getContractConfig } from "../../config";
-import type { SignatureTypeV2, SignedOrderV1, SignedOrderV2 } from "../../order-utils";
+import type { OrderDataV1, SignatureTypeV2, SignedOrderV1, SignedOrderV2 } from "../../order-utils";
 import type { Chain, CreateOrderOptions, UserOrderV2 } from "../../types";
 import { buildOrder } from "./buildOrder";
 import { buildOrderCreationArgs } from "./buildOrderCreationArgs";
@@ -37,7 +37,7 @@ export const createOrder = async (
 				? contractConfig.negRiskExchange
 				: contractConfig.exchange;
 			// Add taker field for V1 orders (V1 requires it, V2 does not)
-			(orderData as any).taker = zeroAddress;
+			(orderData as OrderDataV1).taker = zeroAddress;
 			break;
 		case 2:
 			exchangeContract = options.negRisk
