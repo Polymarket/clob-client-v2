e4044ce525e637969c6bc1f153ef6e1e6af6a677
pradeep-selva <pradeepselvaraju@gmail.com>
2026-02-26 17:44:24 +0530

    rf: fix examples for builders

--- examples/getBuilderTrades.ts
+++ examples/getBuilderTrades.ts
@@ -2,10 +2,8 @@ import { ethers } from "ethers";
 import { config as dotenvConfig } from "dotenv";
 import { resolve } from "path";
 import { ApiKeyCreds, Chain, ClobClient } from "../src";
-import { BuilderApiKeyCreds, BuilderConfig } from "@polymarket/builder-signing-sdk";
 
 dotenvConfig({ path: resolve(__dirname, "../.env") });
-
 
 async function main() {
     const wallet = new ethers.Wallet(`${process.env.PK}`);
@@ -18,19 +16,7 @@ async function main() {
         secret: `${process.env.CLOB_SECRET}`,
         passphrase: `${process.env.CLOB_PASS_PHRASE}`,
     };
-
-    const builderCreds: BuilderApiKeyCreds = {
-        key: `${process.env.BUILDER_API_KEY}`,
-        secret: `${process.env.BUILDER_SECRET}`,
-        passphrase: `${process.env.BUILDER_PASS_PHRASE}`,
-    };
-    const builderConfig = new BuilderConfig({
-        localBuilderCreds: builderCreds
-    });
-    // const builderConfig = new BuilderConfig({
-    //     remoteBuilderSignerUrl: "http://localhost:8080/sign"
-    // });
-    const clobClient = new ClobClient(host, chainId, wallet, creds, undefined, undefined, undefined, false, builderConfig);
+    const clobClient = new ClobClient(host, chainId, wallet, creds);
 
     const trades = await clobClient.getBuilderTrades();
     console.log(trades);
